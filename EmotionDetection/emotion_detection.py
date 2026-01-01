b9ee036cf8d313ace32c5a395bce74498a5eb4c1
Herve MARTINOT <h.martinot13@gmail.com>
2025-12-31 21:25:39 -0500

    commit final project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,43 +4,40 @@ import json
 def emotion_detector(text_to_analyse):
     # URL of the sentiment analysis service
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    
     # Constructing the request payload in the expected format
     myobj = { "raw_document": { "text": text_to_analyse } }
-    
     # Custom header specifying the model ID for the sentiment analysis service
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    
     # Sending a POST request to the sentiment analysis API
     response = requests.post(url, json=myobj, headers=header)
-
     # Parsing the JSON response from the API
     formatted_response = json.loads(response.text)
-    
-    # Extracting sentiment label and score from the response
     # If the response status code is 200, extract the label and score from the response
     if response.status_code == 200:
-         emotions = formatted_response['emotionPredictions'][0]['emotion']
-         #anger_score = emotions['anger']
-         #print(f"emotion anger is: {anger_score}")
-         #disgust_score = emotions['disgust']
-         #fear_score = emotions['fear']
-         #joy_score = emotions['joy']
-         #print(f"emotion joy is: {joy_score}")
-         #sadness_score = emotions['sadness']
-         #dominante_emotion = max(anger_score, disgust_score, fear_score, joy_score, sadness_score)
-         #print(f"The dominante value is {dominante_emotion}")
-         score = max( emotions.items(), key=lambda item: item[1] )
+         myemotions = formatted_response['emotionPredictions'][0]['emotion']
+         anger_score = myemotions['anger']
+         disgust_score = myemotions['disgust']
+         fear_score = myemotions['fear']
+         joy_score = myemotions['joy']
+         sadness_score = myemotions['sadness']
+         score = max( myemotions.items(), key=lambda item: item[1] )
          print(f"dominante_emotion : {score}")
+    elif  response.status_code == 400:
+        return {
+                "anger": None,
+                "disgust": None,
+                "fear": None,
+                "joy": None,
+                "sadness": None,
+                "dominant_emotion": None
+               }       
     else:
-         print(f"status code is: {response.status_code}") 
+         print(f"status code is: {myemotions.status_code}") 
     return {
-                'emotions': emotions.items(),
-                'dominant_emotion': (([score]))
-                #'anger': anger_score,
-                #'disgust': disgust_score,
-                #'fear': fear_score,
-                #'joy': joy_score,
-                #'sadness': sadness_score,
-                #'dominant_emotion': dominante_emotion
+                "anger": anger_score,
+                "disgust": disgust_score,
+                "fear": fear_score,
+                "joy": joy_score,
+                "sadness": sadness_score,
+                "dominant_emotion": score[0]
             }
